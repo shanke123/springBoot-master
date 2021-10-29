744502c222ec94cbe3a29d28d05a5e0072f18a2a
shanke123 <1401231620@qq.com>
2021-10-29 17:25:09 +0800

    init

---
+++ springBoot-master/_springboot-uniapp-mui/src/main/java/com/smart/_springbootuniappmui/SpringbootUniappMuiApplication.java
@@ -0,0 +1,13 @@
+package com.smart._springbootuniappmui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUniappMuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUniappMuiApplication.class, args);
+    }
+
+}
